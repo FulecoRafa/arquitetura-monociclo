95c6a463fe93cf602390eb000ddc134efecd51a0
Rafael Oliveira <ra.pha@live.com>
2020-05-15 10:52:52 -0300

    floats

--- src/components/Hyperlink.vue
+++ src/components/Hyperlink.vue
@@ -1,16 +1,23 @@
 <template>
-  <a :href="to" target="_blank"><slot></slot></a>
+  <span class="linkWrap">
+      <a :href="to" target="_blank"><slot></slot></a>
+      <div class="hover" v-if="info">{{info}}</div>
+  </span>
 </template>
 
 <script>
 export default {
     props: {
-        to: String
+        to: String,
+        info: String
     }
 }
 </script>
 
 <style scoped>
+    .linkWrap{
+        position: relative;
+    }
     a{
         color: white;
         text-decoration: none;
@@ -21,5 +28,20 @@ export default {
     a::after{
         content: '🔗';
         padding: 0 3px;
+    }
+    .hover{
+        display: none;
+        border: 2px solid green;
+        width: 200px;
+        padding: 10px;
+        position: absolute;
+        background: black;
+        z-index: 100;
+        left: 120%;
+        top: 50%;
+        transform: translate(0, -100%);
+    }
+    a:hover + .hover{
+        display: inline;
     }
 </style>
